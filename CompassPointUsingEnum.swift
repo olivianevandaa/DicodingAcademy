de28fd33c6d95b8a71a0dbec03f9ac5e42e90355
Olivia Nevanda Hasibuan <77237351+olivianevandaa@users.noreply.github.com>
2021-08-29 14:50:17 +0700

    Create CompassPointUsingEnum.swift

---
+++ CompassPointUsingEnum.swift
@@ -0,0 +1,9 @@
+enum Planet { case mercury, venus, earth, mars, jupiter, saturn, uranus, neptune }
+
+let somePlanet = Planet.earth
+switch somePlanet {
+case .earth:
+   print("Mostly harmless")
+default:
+   print("Not a safe place for humans")
+}
