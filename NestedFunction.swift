b1e5dcbf1b661c3a89d0cd6b17d795db4e85008e
Olivia Nevanda Hasibuan <77237351+olivianevandaa@users.noreply.github.com>
2021-08-29 14:27:20 +0700

    Create NestedFunction.swift

---
+++ NestedFunction.swift
@@ -0,0 +1,8 @@
+func chooseStepFunction(backward: Bool) -> (Int) -> Int {
+   func stepForward(input: Int) -> Int { return input + 1 }
+   func stepBackward(input: Int) -> Int { return input - 1 }
+   return backward ? stepBackward : stepForward
+}
+
+var currentValue = -4
+let moveNearerToZero = chooseStepFunction(backward: currentValue > 0)
